488bf722c62564c700c8977ce97144440ca8a65d
Jayon Kim <jpkim921@gmail.com>
2018-05-28 18:11:29 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201201114) do
+ActiveRecord::Schema.define(version: 20180528180816) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
